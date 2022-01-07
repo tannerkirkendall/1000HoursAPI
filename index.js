46e514323bd1897a2f75e2f1ab6ebf3df83043e5
Tanner <tkirkendall@trshealthcare.com>
2022-01-07 11:09:16 -0600

    update

--- index.js
+++ index.js
@@ -15,9 +15,8 @@ mongoose.connect(
     }
 );
 
-
 app.use(express.json());
 app.use('/api/user', authRoute);
 app.use('/api/posts', postRoute);
 
-app.listen(3000, () => console.log("Server Running"));
+app.listen(3001, () => console.log("Server Running"));
