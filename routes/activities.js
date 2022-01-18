49f0b1d4b4cf7c9c32b5f4efd2ce69dda4d3482d
Tanner <tkirkendall@trshealthcare.com>
2022-01-18 13:44:28 -0600

    updates

--- routes/activities.js
+++ routes/activities.js
@@ -4,13 +4,15 @@ const User = require('../models/User');
 const calc = require('../calculations');
 
 router.post('/', verify, async (req, res) => {
-
     try{
-        await User.findOneAndUpdate(
+        var asdf = await User.findOneAndUpdate(
             {_id: req.user._id},
-            { $push: { activties: { startTime: req.body.startTime, endTime: req.body.endTime } } }
+            { $push: { activities: { startTime: req.body.startTime, endTime: req.body.endTime } } },
+            {
+                new: true
+            }
         );
-        res.send();
+        res.send(asdf.activities);
     }catch (err){
         res.status(400).send(err);
     }
@@ -21,8 +23,8 @@ router.patch('/:activityId', verify, async (req, res) => {
     
     try{
         var asdf = await User.updateOne(
-            {_id: req.user._id, "activties._id": req.params.activityId},
-            { $set: { "activties.$.startTime": req.body.startTime, "activties.$.endTime": req.body.endTime } }
+            {_id: req.user._id, "activities._id": req.params.activityId},
+            { $set: { "activities.$.startTime": req.body.startTime, "activities.$.endTime": req.body.endTime } }
         );
         res.send(asdf);
     }catch (err){
